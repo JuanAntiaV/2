6c0ca4860e97c6babec637eb6187bceb748cbe48
Juan Antia <juanmartinantia@gmail.com>
2023-08-30 18:39:39 -0500

    first commit

---
+++ react.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import Menu from './Menu';
+import './App.css';
+
+function App(
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi Tienda en Línea</h1>
+      </header>
+
+   
+   
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi Tienda en Línea</h1>
+      </header>
+      <Menu />
+      <main className="App-content">
+      </main>
+  )
+)
+      function CartWidget( 
+  return (
+    
+    <div className="cart-widget">
+
+      <i className="fas fa-shopping-cart"></i>
+
+      <span className="cart"
+
+       <div className="cart-widget>
+
+      <i className="fas fa-shopping-cart"></i>
+      <span className="cart-count">3</span>
+    </div>
+  )
+      )
+
+      import Catalog from './Catalog';
+import './App.css';
+
+function App( {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        
+<div className="App">
+      <header className="App-header">
+        <h1>Tienda de Bicicletas</h1>
+      </header>
+      <Catalog />
+    </div>
+  }
+)
+
+export default App;
+
+mport BikeCard from './BikeCard';
+import './Catalog.css';
+
+const bikeData = [
+  { id: 1, name: 'Bicicleta de Montaña', type: 'Montaña', price: '$500' },
+  
+  {
+id: 2, name: 'Bicicleta de Ciudad', type: 'Urbana', price: '$300' },
+  // Agrega más datos de bicicletas aquí
+];
+
+function Catalog ( {
+  return (
+    
+    <div className="Catalog">
+      <h2>Catálogo de Bicicletas</h2>
+      <div className="BikeList">
+        {bikeData.map((bike) => (
+          <BikeCard key={bike.id} bike={bike} />
+        )
+        )
+        }
+      
+
+    <div className="Catalog">
+      <h2>Catálogo de Bicicletas</h2>
+      <div className="BikeList">
+        {bikeData.map((bike) => (
+          <BikeCard key={bike.id} bike={bike} />
+        ))}
+     
+
+    <div className="Catalog">
+      <h2>Catálogo de Bicicletas</h2>
+      <div className="BikeList">
+        {bikeData.map((bike)
+          <BikeCard key={bike.id} bike={bike
+
+    <div className="Catalog">
+      <h2>Catálogo de Bicicletas</h2>
+      <div className="BikeList">
+        {bikeData.map((bike) =>
+
+    <div className="Catalog">
+      <h2>Catálogo de Bicicletas</h2>
+      <div className="Bike>
+
